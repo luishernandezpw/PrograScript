2832dc8c0d16eb15f9758b01a2de08df7ee09a86
Luis Hernandez <sarco_luis@hotmail.com>
2024-03-15 01:07:44 +0000

    Creacion de un hola mundo

--- intro.sh
+++ intro.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-nombre="Luis Hernandez"
-edad=15
 
-echo "Hola, $nombre; tu edad es: $edad"
-
-if [ $edad -ge 18 ]; then
-    echo "$nombre, eres mayor de edad"
-else
-    echo "$nombre, eres menor de edad"
-fi
+echo "Hola Mundo"
