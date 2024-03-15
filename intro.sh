c83ef166a50be20e4beaaa61205ccca0bcba3d15
Luis Hernandez <sarco_luis@hotmail.com>
2024-03-15 02:17:41 +0000

    Se agrego las opciones de porcentaje y exponenciacion

--- intro.sh
+++ intro.sh
@@ -9,6 +9,9 @@ echo "1. Suma"
 echo "2. Resta"
 echo "3. Multiplicacion"
 echo "4. Division"
+echo "5. Porcentaje"
+echo "6. Exponenciacion"
+
 read -p "Opcion: " opcion
 respuesta=0
 if [ $opcion = 1 ]; then
@@ -19,5 +22,10 @@ elif [ $opcion = 3 ]; then
     respuesta=$((num1*num2))
 elif [ $opcion = 4 ]; then
     respuesta=$((num1/num2))
+elif [ $opcion = 5 ]; then
+    respuesta=$((num1*num2/100))
+elif [ $opcion = 6 ]; then
+    respuesta=$((num1**num2))
+    #respuesta=$(echo "$num1 ^ $num2" | bc)
 fi 
 echo "La respuesta es: $respuesta"
