ab823b028b4c489ad4a8679ac33aa45c3e8a6273
Luis Hernandez <sarco_luis@hotmail.com>
2024-03-20 01:57:50 +0000

    Desarrollo de ejercicios con un MENU

--- bloque.sh
+++ bloque.sh
@@ -1,9 +1,30 @@
 
-seguir=1
-while [ $seguir -eq 1 ]; do
-    read -p "Tabla de multiplicar: " ntabla
-    for i in $(seq 1 10); do
-        echo "$ntabla x $i = $(($ntabla*$i))"
-    done
-    read -p "Continuar: 1; Salir: Cualquier otro numero o letra: " seguir
+continuar=1
+while [ $continuar -eq 1 ]; do
+    echo "::. MENU .::"
+    echo "1. Mayor 5 numeros."
+    echo "2. Tabla de multiplicar."
+    echo "3. Salir"
+    read -p "Opcion: " opcion
+
+    case $opcion in
+        1)
+            echo "Mayor de 5 Numeros"
+            mayor=0
+            for i in $(seq 1 5); do
+                read -p "Numero: " num
+                if [ $num -ge $mayor ]; then
+                    mayor=$num
+                fi
+            done
+            echo "El numero mayor es: $mayor"
+            ;;
+        2)
+            read -p "Tabla de multiplicar: " ntabla
+            for i in $(seq 1 10); do
+                echo "$ntabla x $i = $(($ntabla*$i))"
+            done
+            ;;
+        3) continuar=2
+    esac
 done
